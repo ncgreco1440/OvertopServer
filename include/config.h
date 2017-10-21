958d81720f9eafb839cf0c406bf7ccc9bd905eca
Nico <ncgreco1440@gmail.com>
2017-10-21 15:57:35 -0400

    includes

---
+++ include/config.h
@@ -0,0 +1,9 @@
+#include <statements.h>
+
+#ifndef _WIN32_
+#define _WIN32_
+#endif
+
+#ifndef _VERSION_
+#define _VERSION_ 1
+#endif
---
+++ include/statements.h
@@ -0,0 +1,8 @@
+namespace Overtop
+{
+    namespace Messages
+    {
+        const char* SVR_START_MSG = "Overtop Server Started";
+        const char* SVR_STOP_MSG = "Server Stopped";
+    }
+}
